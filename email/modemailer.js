c2a7b0351c35c76768a6e244e375f9801a62cfd0
anadulimited@gmail.com <anadulimited@gmail.com>
2025-09-26 22:41:08 +0100

    Update email transporter configuration to use service instead of host

--- email/modemailer.js
+++ email/modemailer.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 
 // Create a test account or replace with real credentials.
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  service: "gmail",
   port: 587,
   secure: false, // true for 465, false for other ports
   auth: {
