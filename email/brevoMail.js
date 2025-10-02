1e0d45175348d7062d809c5a3e257cd7190f5779
anadulimited@gmail.com <anadulimited@gmail.com>
2025-10-02 04:28:24 +0100

    corrected valid email

--- email/brevoMail.js
+++ email/brevoMail.js
@@ -7,7 +7,7 @@ const sendEmail = async (options) => {
     const response = await axios.post(
       "https://api.brevo.com/v3/smtp/email",
       {
-        sender: { email:`<DawnEats ${process.env.EMAIL}>`, name: "DawnEats" },
+        sender: { email:`${process.env.EMAIL}`, name: "DawnEats" },
         to: [{ email: options.email }],
         subject: options.subject,
         htmlContent: options.html,
@@ -20,6 +20,9 @@ const sendEmail = async (options) => {
         },
       }
     );
+    if (response.data.error) {
+      throw new Error(response.data.error.message);
+    }
     console.log("Email sent successfully:", response.data);
   } catch (error) {
     console.error(
