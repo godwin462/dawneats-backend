93b937eae5ee71d515608e8c20ad70dbf9c2e4f7
anadulimited@gmail.com <anadulimited@gmail.com>
2025-10-02 04:13:32 +0100

    Update swagger configuration

--- swagger.js
+++ swagger.js
@@ -1,19 +1,26 @@
 const swaggerAutogen = require("swagger-autogen")({ openapi: "3.0.0" });
+const env = process.env.NODE_ENV || "development";
+
+const endpointsFiles = ["./index.js"];
 
 const doc = {
   info: {
     title: "DawnEats API",
-    description: "Description",
+    description: "DawnEats API Documentation",
+    version: "1.0.0",
   },
-  host: "dawneats-backend.onrender.com",
+  host:
+    env === "production"
+      ? "https://dawneats-backend.onrender.com"
+      : "http://localhost:8080",
+  basePath: "/",
+  schemes: ["https", "http"],
+  consumes: ["application/json"],
+  produces: ["application/json"],
 };
 
 const outputFile = "./swagger.json";
-const routes = [
-  "./index.js",
-];
-
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
 
-swaggerAutogen(outputFile, routes, doc);
+swaggerAutogen(outputFile, endpointsFiles, doc);
